1d35f5c312f1f3f6a5f849650e02bcb2a6497cb7
valli450 <v.dranov450@gmail.com>
2024-11-21 15:08:50 -0500

    Updated version of the website

--- scripts/main.js
+++ scripts/main.js
@@ -80,17 +80,14 @@ function showHero() {
 // Cookie check
 function hasOneHourPassed() {
   const lastVisit = getCookie("lastVisit");
-  console.log(lastVisit);
   if (lastVisit) {
       const currentTime = new Date().getTime();
       const timeDifference = currentTime - lastVisit;
-      console.log("Time difference in milliseconds: ", timeDifference);
       return timeDifference > 3600000; 
   }
 
   if (timeDifference > 3600000 == true) {
     
-    console.log("True")
     return true
   }
   
@@ -101,16 +98,13 @@ function setCookie(name, value, hours) {
   const date = new Date();
   date.setTime(date.getTime() + (hours * 60 * 60 * 1000));
   const expires = "expires=" + date.toUTCString();
-  console.log("Coockie creations: " + name + "=" + value + ";" + expires + ";path=/")
   document.cookie = name + "=" + value + ";" + expires + ";path=/";
-  console.log(document.cookie)
 }
 
 // Coockie return
 function getCookie(name) {
   const nameCockie = name + "=";
   const ca = document.cookie.split(';');
-  console.log(ca)
   for (let i = 0; i < ca.length; i++) {
       let c = ca[i];
       while (c.charAt(0) == ' ') c = c.substring(1);
@@ -124,17 +118,28 @@ function getCookie(name) {
 document.addEventListener("DOMContentLoaded", () => {
   const burgerMenu = document.getElementById("burgerMenu");
   const navMenu = document.getElementById("navMenu");
+  const logoLink = document.getElementById("logoLink");
+  const zIndex = window.getComputedStyle(logoLink).zIndex;
 
   burgerMenu.addEventListener("click", () => {
-      document.body.classList.toggle("modal-active")
+      document.body.classList.toggle("modal-active");
       navMenu.classList.toggle("open");
-
+      if (zIndex === "10") {
+        logoLink.style.zIndex = "0";
+      } else{
+        logoLink.style.zIndex = "10";
+      }
   });
 
   document.addEventListener("click", (e) => {
       if (!navMenu.contains(e.target) && !burgerMenu.contains(e.target)) {
           navMenu.classList.remove("open");
           document.body.classList.remove("modal-active");
+          if (zIndex === "10") {
+            logoLink.style.zIndex = "0";
+          } else{
+            logoLink.style.zIndex = "10";
+          }
       }
   });
 });
